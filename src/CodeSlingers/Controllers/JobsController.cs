c46d4c2602135b79e9140dafe5af6ada1df9c7cb
Todd H. Gardner <todd@wocketware.com>
2012-11-17 16:17:05 -0600

    added restsharp for jobscontroller

---
+++ src/CodeSlingers/Controllers/JobsController.cs
@@ -0,0 +1,34 @@
+﻿using CodeSlingers.Models;
+using RestSharp;
+using RestSharp.Deserializers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CodeSlingers.Controllers
+{
+    public class JobsController : Controller
+    {
+        //
+        // POST: /Jobs/
+        [HttpPost]
+        public JsonResult Index(Job job)
+        {
+            var client = new RestClient("http://vermillion.howard.fusionroomdev.com");
+            var request = new RestRequest("api/vermillion", Method.POST);
+            request.RequestFormat = DataFormat.Json;
+            request.AddBody(job);
+            
+
+            // execute the request
+            var response = client.Execute<Job>(request);
+            
+            this.Response.StatusCode = (int) response.StatusCode;
+            return this.Json(response.Data, JsonRequestBehavior.DenyGet);
+        }
+    }
+}
+
