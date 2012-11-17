fba364a52e882f649cc704029ac28071bdb628a7
Ryan Tofteland <ryantofteland@gmail.com>
2012-11-17 07:31:07 -0600

    map it like its hot

--- src/CodeSlingers/Js/maps.js
+++ src/CodeSlingers/Js/maps.js
@@ -1,1 +1,17 @@
-﻿
+﻿$(function () {
+	var _googleMap;
+	var _mapIcon = "";
+
+	var _initializeGoogleMap = function () {
+		var latlng = new google.maps.LatLng(44.960195, -93.164663);
+		var myOptions = {
+			zoom: 11,
+			center: latlng,
+			mapTypeId: google.maps.MapTypeId.ROADMAP
+		};
+		_googleMap = new google.maps.Map($("#map-canvas")[0], myOptions);
+		//_loadSchoolMarkers();
+	};
+
+	_initializeGoogleMap();
+});
