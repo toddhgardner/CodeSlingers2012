a9053f9dacb07b536cc8b625f2c79da87c94f991
Todd H. Gardner <todd@wocketware.com>
2012-11-17 10:51:50 -0600

    menu pagination

--- src/CodeSlingers/Js/menu.js
+++ src/CodeSlingers/Js/menu.js
@@ -1,8 +1,23 @@
-﻿//$(document).ready(function () {
-//    $("#menu").slides({
-//        container: 'menu-items',
-//        pause: 5000,
-//        generateNextPrev: false,
-//        generatePagination: false
-//    });
-//});
+﻿$(document).ready(function () {
+    var startSlide = 1;
+    if (window.location.hash) {
+        startSlide = window.location.hash.replace('#', '');
+    }
+
+    $("#menu").slides({
+        preload:true,
+        start: startSlide,
+        container: 'menu-items',
+        pause: 5000,
+        //currentClass: 'active',
+        //pagination: 'menu-links',
+        next: 'menu-right',
+        prev: 'menu-left',
+        generateNextPrev: false,
+        //generatePagination: false,
+        animationComplete: function (current) {
+            // Set the slide number as a hash
+            window.location.hash = '#' + current;
+        }
+    });
+});
