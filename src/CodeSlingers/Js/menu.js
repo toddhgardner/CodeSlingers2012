9e2963c8307d1221661590eab8a956c85432606b
Todd H. Gardner <todd@wocketware.com>
2012-11-17 10:14:03 -0600

    menu and css changes

--- src/CodeSlingers/Js/menu.js
+++ src/CodeSlingers/Js/menu.js
@@ -1,1 +1,8 @@
-﻿
+﻿//$(document).ready(function () {
+//    $("#menu").slides({
+//        container: 'menu-items',
+//        pause: 5000,
+//        generateNextPrev: false,
+//        generatePagination: false
+//    });
+//});
